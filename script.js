bd8c9dd1a961f6446fb260d891e04f6f7ac87812
David De La Vega <ddelavega2015@gmail.com>
2022-06-02 21:08:01 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const PI = 3.14
+
+/* 1. Var can be redeclared while let cannot be redeclared. Var will hoist to the top of the scope while let creates a block-scope
+2. Var can be reassigned and redeclared while const cannot be reassigned or redeclared. Var will hoist to the top of the scope while const creates a block-scope
+3. Let can be reassigned while const cannot be reassigned
+4. Hoisting is when a variable is pulled to the top of the scope.
+*/
